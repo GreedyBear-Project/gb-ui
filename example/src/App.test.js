9d61d48eef3e26e66bafe61766398ac49416a314
rootp1 <arnav.iitr@gmail.com>
2026-02-20 13:12:39 +0530

    remove react-scripts

--- example/src/App.test.js
+++ example/src/App.test.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  document.body.appendChild(div);
-  const root = createRoot(div);
-  root.render(<App />);
-  root.unmount();
-  div.remove();
+  render(<App />);
 });
